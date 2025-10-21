caae903c340e928455bd1ca6098e1cdee66feac6
Ungeziefi <163609976+Ungeziefi@users.noreply.github.com>
2025-10-21 22:57:51 +0200

    CustomInventorySize breaks DimUnallowedItems, disabling for now

--- New Mods/Container Utilities/CustomInventorySize.cs
+++ New Mods/Container Utilities/CustomInventorySize.cs
@@ -1,77 +1,79 @@
-using System.Collections.Generic;
-using System.Reflection.Emit;
-using HarmonyLib;
-using Ungeziefi.Container_Utilities.UI;
-using UnityEngine.UI;
+// To-Do: Fix it breaking DimUnallowedItems
 
-namespace Ungeziefi.Container_Utilities
-{
-    [HarmonyPatch]
-    public class CustomInventorySize
-    {
-        private static ScrollRect scrollRect;
-        private const int VANILLA_WIDTH = 6;
-        private const int VANILLA_HEIGHT = 8;
+//using System.Collections.Generic;
+//using System.Reflection.Emit;
+//using HarmonyLib;
+//using Ungeziefi.Container_Utilities.UI;
+//using UnityEngine.UI;
 
-        #region Patches
-        [HarmonyPatch(typeof(Inventory), nameof(Inventory.Awake)), HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Inventory_Awake(IEnumerable<CodeInstruction> instructions)
-        {
-            if (Main.Config.InventoryWidth == VANILLA_WIDTH && Main.Config.InventoryHeight == VANILLA_HEIGHT)
-                return instructions;
+//namespace Ungeziefi.Container_Utilities
+//{
+//    [HarmonyPatch]
+//    public class CustomInventorySize
+//    {
+//        private static ScrollRect scrollRect;
+//        private const int VANILLA_WIDTH = 6;
+//        private const int VANILLA_HEIGHT = 8;
 
-            var matcher = new CodeMatcher(instructions);
+//        #region Patches
+//        [HarmonyPatch(typeof(Inventory), nameof(Inventory.Awake)), HarmonyTranspiler]
+//        public static IEnumerable<CodeInstruction> Inventory_Awake(IEnumerable<CodeInstruction> instructions)
+//        {
+//            if (Main.Config.InventoryWidth == VANILLA_WIDTH && Main.Config.InventoryHeight == VANILLA_HEIGHT)
+//                return instructions;
 
-            // Replace sizes
-            matcher.MatchForward(false,
-                new CodeMatch(OpCodes.Ldc_I4_6),
-                new CodeMatch(OpCodes.Ldc_I4_8)
-            );
+//            var matcher = new CodeMatcher(instructions);
 
-            matcher.SetInstruction(new CodeInstruction(OpCodes.Call,
-                AccessTools.Method(typeof(CustomInventorySize), nameof(GetInventoryWidth))));
+//            // Replace sizes
+//            matcher.MatchForward(false,
+//                new CodeMatch(OpCodes.Ldc_I4_6),
+//                new CodeMatch(OpCodes.Ldc_I4_8)
+//            );
 
-            matcher.Advance(1).SetInstruction(new CodeInstruction(OpCodes.Call,
-                AccessTools.Method(typeof(CustomInventorySize), nameof(GetInventoryHeight))));
+//            matcher.SetInstruction(new CodeInstruction(OpCodes.Call,
+//                AccessTools.Method(typeof(CustomInventorySize), nameof(GetInventoryWidth))));
 
-            return matcher.InstructionEnumeration();
-        }
+//            matcher.Advance(1).SetInstruction(new CodeInstruction(OpCodes.Call,
+//                AccessTools.Method(typeof(CustomInventorySize), nameof(GetInventoryHeight))));
 
-        [HarmonyPatch(typeof(uGUI_ItemsContainer), nameof(uGUI_ItemsContainer.Init)), HarmonyPostfix]
-        public static void uGUI_ItemsContainer_Init(uGUI_ItemsContainer __instance, ItemsContainer ___container)
-        {
-            if (!ShouldApplyCustomSize(__instance)) return;
+//            return matcher.InstructionEnumeration();
+//        }
 
-            if (Main.Config.InventoryHeight > VANILLA_HEIGHT)
-            {
-                scrollRect = ScrollbarUtility.CreateScrollView(__instance, ___container, VANILLA_HEIGHT);
-            }
-        }
+//        [HarmonyPatch(typeof(uGUI_ItemsContainer), nameof(uGUI_ItemsContainer.Init)), HarmonyPostfix]
+//        public static void uGUI_ItemsContainer_Init(uGUI_ItemsContainer __instance, ItemsContainer ___container)
+//        {
+//            if (!ShouldApplyCustomSize(__instance)) return;
 
-        [HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnOpenPDA)), HarmonyPostfix]
-        public static void uGUI_InventoryTab_OnOpenPDA()
-        {
-            if (!Main.Config.EnableCustomInventorySize || scrollRect == null)
-                return;
+//            if (Main.Config.InventoryHeight > VANILLA_HEIGHT)
+//            {
+//                scrollRect = ScrollbarUtility.CreateScrollView(__instance, ___container, VANILLA_HEIGHT);
+//            }
+//        }
 
-            // Reset scroll to top on open
-            scrollRect.verticalNormalizedPosition = 1;
-        }
-        #endregion
+//        [HarmonyPatch(typeof(uGUI_InventoryTab), nameof(uGUI_InventoryTab.OnOpenPDA)), HarmonyPostfix]
+//        public static void uGUI_InventoryTab_OnOpenPDA()
+//        {
+//            if (!Main.Config.EnableCustomInventorySize || scrollRect == null)
+//                return;
 
-        #region Helper Methods
-        private static bool ShouldApplyCustomSize(uGUI_ItemsContainer instance)
-        {
-            return Main.Config.EnableCustomInventorySize &&
-                   instance.transform.name == "InventoryContainer" &&
-                   (Main.Config.InventoryWidth != VANILLA_WIDTH || Main.Config.InventoryHeight != VANILLA_HEIGHT);
-        }
+//            // Reset scroll to top on open
+//            scrollRect.verticalNormalizedPosition = 1;
+//        }
+//        #endregion
 
-        private static int GetInventoryWidth() =>
-            Main.Config.EnableCustomInventorySize ? Main.Config.InventoryWidth : VANILLA_WIDTH;
+//        #region Helper Methods
+//        private static bool ShouldApplyCustomSize(uGUI_ItemsContainer instance)
+//        {
+//            return Main.Config.EnableCustomInventorySize &&
+//                   instance.transform.name == "InventoryContainer" &&
+//                   (Main.Config.InventoryWidth != VANILLA_WIDTH || Main.Config.InventoryHeight != VANILLA_HEIGHT);
+//        }
 
-        private static int GetInventoryHeight() =>
-            Main.Config.EnableCustomInventorySize ? Main.Config.InventoryHeight : VANILLA_HEIGHT;
-        #endregion
-    }
-}
+//        private static int GetInventoryWidth() =>
+//            Main.Config.EnableCustomInventorySize ? Main.Config.InventoryWidth : VANILLA_WIDTH;
+
+//        private static int GetInventoryHeight() =>
+//            Main.Config.EnableCustomInventorySize ? Main.Config.InventoryHeight : VANILLA_HEIGHT;
+//        #endregion
+//    }
+//}
