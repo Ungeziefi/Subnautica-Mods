7518cc0c016c0a51164a23733813437d290e9f25
Ungeziefi <163609976+Ungeziefi@users.noreply.github.com>
2025-12-13 17:47:22 +0100

    Cleanup

--- T&F Rework/Tweaks/ManualTorpedoSelection.cs
+++ T&F Rework/Tweaks/ManualTorpedoSelection.cs
@@ -7,7 +7,7 @@ namespace Ungeziefi.Tweaks
     [HarmonyPatch]
     public class ManualTorpedoSelection
     {
-        private static Dictionary<string, TechType> selectedTorpedoTypes = new();
+        private static readonly Dictionary<string, TechType> selectedTorpedoTypes = new();
 
         // Selection cooldown
         private static float lastSelectionTime = 0f;
@@ -84,34 +84,30 @@ namespace Ungeziefi.Tweaks
             }
 
             // Get current selection or select first torpedo
-            if (!selectedTorpedoTypes.TryGetValue(vehicleId, out TechType currentSelection))
+            TechType nextTorpedo;
+            if (selectedTorpedoTypes.TryGetValue(vehicleId, out TechType currentSelection))
             {
-                selectedTorpedoTypes[vehicleId] = availableTorpedos[0];
-                ShowSelectedTorpedoMessage(availableTorpedos[0], vehicle);
-                return;
+                int currentIndex = availableTorpedos.IndexOf(currentSelection);
+                // If current selection is not found, or is invalid, default to first
+                if (currentIndex == -1)
+                    currentIndex = -1; // Will become 0 after increment
+                
+                int nextIndex = (currentIndex + 1) % availableTorpedos.Count;
+                nextTorpedo = availableTorpedos[nextIndex];
+            }
+            else
+            {
+                nextTorpedo = availableTorpedos[0];
             }
 
-            // Find current torpedo in available list
-            int currentIndex = availableTorpedos.IndexOf(currentSelection);
-            if (currentIndex == -1)
-            {
-                selectedTorpedoTypes[vehicleId] = availableTorpedos[0];
-                ShowSelectedTorpedoMessage(availableTorpedos[0], vehicle);
-                return;
-            }
-
-            // Select next torpedo
-            int nextIndex = (currentIndex + 1) % availableTorpedos.Count;
-            selectedTorpedoTypes[vehicleId] = availableTorpedos[nextIndex];
-            ShowSelectedTorpedoMessage(availableTorpedos[nextIndex], vehicle);
+            selectedTorpedoTypes[vehicleId] = nextTorpedo;
+            ShowSelectedTorpedoMessage(nextTorpedo);
         }
 
         // Show torpedo selection message
-        private static void ShowSelectedTorpedoMessage(TechType torpedoType, Vehicle vehicle)
+        private static void ShowSelectedTorpedoMessage(TechType torpedoType)
         {
-            string vehicleName = vehicle is SeaMoth ? "Seamoth" : "Exosuit";
-            string torpedoName = Language.main.Get(torpedoType);
-            ErrorMessage.AddMessage($"{torpedoName} selected");
+            ErrorMessage.AddMessage($"{Language.main.Get(torpedoType)} selected");
         }
 
         // Seamoth torpedo selection input
@@ -138,7 +134,7 @@ namespace Ungeziefi.Tweaks
 
         // Override torpedo firing to use selected type
         [HarmonyPatch(typeof(Vehicle), nameof(Vehicle.TorpedoShot)), HarmonyPrefix]
-        public static bool Vehicle_TorpedoShot(Vehicle __instance, ItemsContainer container, ref TorpedoType torpedoType, Transform muzzle, ref bool __result)
+        public static bool Vehicle_TorpedoShot(Vehicle __instance, ItemsContainer container, ref TorpedoType torpedoType)
         {
             if (!Main.Config.TCEnableFeature || container == null)
                 return true;
@@ -148,7 +144,6 @@ namespace Ungeziefi.Tweaks
 #pragma warning disable Harmony003 // Harmony non-ref patch parameters modified
                 __instance = Player.main.currentMountedVehicle;
 #pragma warning restore Harmony003 // Harmony non-ref patch parameters modified
-
             if (__instance == null)
                 return true;
 
