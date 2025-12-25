712a034eaf7c1040e9462f7e3f397c3a647ebc78
Ungeziefi <163609976+Ungeziefi@users.noreply.github.com>
2025-12-25 03:35:43 +0100

    Custom Hotkeys Remake 1.4.0

--- New Mods/Custom Hotkeys Remake/Main.cs
+++ New Mods/Custom Hotkeys Remake/Main.cs
@@ -13,7 +13,7 @@ namespace Ungeziefi.Custom_Hotkeys_Remake
     {
         public const string PLUGIN_GUID = "Ungeziefi.Custom_Hotkeys_Remake";
         public const string PLUGIN_NAME = "Custom Hotkeys Remake";
-        public const string PLUGIN_VERSION = "1.3.0";
+        public const string PLUGIN_VERSION = "1.4.0";
 
         private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
         internal static new ManualLogSource Logger { get; private set; }
@@ -28,7 +28,8 @@ namespace Ungeziefi.Custom_Hotkeys_Remake
             RefreshHotkeysButton = EnumHandler.AddEntry<GameInput.Button>("RefreshHotkeysButton")
                 .CreateInput("Refresh hotkeys")
                 .WithKeyboardBinding(InputPaths.Keyboard.F9)
-                .WithCategory("Custom Hotkeys Remake");
+                .WithCategory("Custom Hotkeys Remake")
+                .AvoidConflicts();
 
             Harmony.CreateAndPatchAll(Assembly, $"{PLUGIN_GUID}");
         }
