89c87db7a91ec5ea0b9991323b8446f4571a205c
jbeast291 <75239008+jbeast291@users.noreply.github.com>
2026-02-01 22:58:07 -0800

    Migrate OxygenPipesAcrossSubBiomes to a transpiler

--- T&F Rework/Fixes/OxygenPipesAcrossSubBiomes.cs
+++ T&F Rework/Fixes/OxygenPipesAcrossSubBiomes.cs
@@ -1,32 +1,38 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace Ungeziefi.Fixes
 {
     [HarmonyPatch]
-    internal class OxygenPipesAcrossSubBiomes
+    public class OxygenPipesAcrossSubBiomes
     {
-        [HarmonyPatch(typeof(AtmosphereVolume), nameof(AtmosphereVolume.Start)), HarmonyPostfix]
-        public static void AtmosphereVolume_Start(AtmosphereVolume __instance)
+        /// <summary>
+        /// This ensures that the oxygen pipe placement check hits only colliders that the player would also collide with,
+        /// ignoring anything that is just a trigger.
+        /// </summary>
+        [HarmonyPatch(typeof(OxygenPipe), nameof(OxygenPipe.IsInSight)), HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            if (!Main.Config.OxygenPipesAcrossSubBiomes) return;
+            if (!Main.Config.OxygenPipesAcrossSubBiomes) return instructions;
+            
+            
+            CodeMatcher matcher = new CodeMatcher(instructions);
 
-            // Prevent turning every creature orange in the PCF prison biomes
-            HashSet<string> excludedBiomes = new() {
-                "Prison_Moonpool",
-                "Prison_Antechamber",
-                "Prison_UpperRoom",
-                "Prison_Aquarium_Mid",
-                "Prison_Aquarium_Upper",
-                "Prison_Aquarium",
-                "Prison_Aquarium_Cave"
-                };
+            //Find where it loads 0 onto the stack, this is converted to an enum later considered QueryTriggerInteraction.UseGlobal
+            matcher.MatchForward(true, new CodeMatch(ci => ci.opcode == OpCodes.Ldc_I4_0));
 
-            if (excludedBiomes.Contains(__instance.overrideBiome)) return;
-
-            int noRayCastLayer = LayerMask.NameToLayer("Ignore Raycast");
-            __instance.gameObject.layer = noRayCastLayer;
+            if (!matcher.IsValid)
+            {
+                Main.Logger.LogWarning($"Skipping {nameof(OxygenPipesAcrossSubBiomes)} Transpiler! Likely another mod is modifying OxygenPipe.IsInSight");
+                return matcher.Instructions();
+            }
+			
+            //Set to 1 so the enum resolves to QueryTriggerInteraction.Ignore.
+            matcher.Set(OpCodes.Ldc_I4_1, null);
+		
+            return matcher.InstructionEnumeration();
         }
     }
 }
