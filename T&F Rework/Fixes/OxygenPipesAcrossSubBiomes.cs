fdc57362f741d045c1f9afe943514e15707bbb2c
Osema Cherif <163609976+Ungeziefi@users.noreply.github.com>
2026-02-06 12:20:17 +0100

    OxygenPipesAcrossSubBiomes cleanup

--- T&F Rework/Fixes/OxygenPipesAcrossSubBiomes.cs
+++ T&F Rework/Fixes/OxygenPipesAcrossSubBiomes.cs
@@ -1,37 +1,32 @@
 ﻿using HarmonyLib;
 using System.Collections.Generic;
 using System.Reflection.Emit;
-using UnityEngine;
 
 namespace Ungeziefi.Fixes
 {
     [HarmonyPatch]
     public class OxygenPipesAcrossSubBiomes
     {
-        /// <summary>
-        /// This ensures that the oxygen pipe placement check hits only colliders that the player would also collide with,
-        /// ignoring anything that is just a trigger.
-        /// </summary>
+        // Makes the oxygen pipes collision check ignore colliders that wouldn't affect the player
         [HarmonyPatch(typeof(OxygenPipe), nameof(OxygenPipe.IsInSight)), HarmonyTranspiler]
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             if (!Main.Config.OxygenPipesAcrossSubBiomes) return instructions;
-            
-            
-            CodeMatcher matcher = new CodeMatcher(instructions);
+
+            var matcher = new CodeMatcher(instructions);
 
             //Find where it loads 0 onto the stack, this is converted to an enum later considered QueryTriggerInteraction.UseGlobal
             matcher.MatchForward(true, new CodeMatch(ci => ci.opcode == OpCodes.Ldc_I4_0));
 
             if (!matcher.IsValid)
             {
-                Main.Logger.LogWarning($"Skipping {nameof(OxygenPipesAcrossSubBiomes)} Transpiler! Likely another mod is modifying OxygenPipe.IsInSight");
+                Main.Logger.LogWarning($"Skipping the {nameof(OxygenPipesAcrossSubBiomes)} transpiler patch! Another mod might be modifying OxygenPipe.IsInSight.");
                 return matcher.Instructions();
             }
-			
+
             //Set to 1 so the enum resolves to QueryTriggerInteraction.Ignore.
             matcher.Set(OpCodes.Ldc_I4_1, null);
-		
+
             return matcher.InstructionEnumeration();
         }
     }
