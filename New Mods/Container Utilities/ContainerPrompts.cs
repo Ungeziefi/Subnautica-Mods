b904cb60f895f1483a41340cda086962ac642e3d
Your New Name <163609976+Ungeziefi@users.noreply.github.com>
2026-04-26 18:25:29 +0200

    Add missing Container Utilities files

---
+++ New Mods/Container Utilities/ContainerPrompts.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace Ungeziefi.Container_Utilities;
+
+[HarmonyPatch]
+public class ContainerPrompts
+{
+    private static int GetUsedSlotsCount(ItemsContainer container)
+    {
+        var occupied = 0;
+        foreach (var item in container) occupied += item.width * item.height;
+        return occupied;
+    }
+
+    private static string GetFillColorHex(int used, int total)
+    {
+        var t = Mathf.Clamp01((float)used / total);
+        var statusColor = Color.Lerp(Color.green, Color.red, t);
+        return ColorUtility.ToHtmlStringRGB(statusColor);
+    }
+
+    [HarmonyPatch(typeof(StorageContainer), nameof(StorageContainer.OnHandHover))]
+    [HarmonyPostfix]
+    public static void StorageContainer_OnHandHover(StorageContainer __instance)
+    {
+        if (!Main.Config.ShowStorageInformation) return;
+
+        var used = GetUsedSlotsCount(__instance.container);
+        var total = ItemStorageHelper.GetTotalSlots(__instance.container);
+        var colorHex = GetFillColorHex(used, total);
+
+        var status = used >= total ? " - Full" : used == 0 ? " - Empty" : "";
+        var countText = Main.Config.ColorCodedPrompts
+            ? $"<color=#{colorHex}>{used}/{total}</color>"
+            : $"{used}/{total}";
+
+        HandReticle.main.SetText(HandReticle.TextType.HandSubscript, $"{countText}{status}", true);
+    }
+}
---
+++ New Mods/Container Utilities/TrashcanAndWFMPatches.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Ungeziefi.Container_Utilities;
+
+[HarmonyPatch]
+public class TrashcanAndWFMPatches
+{
+    #region Trashcan
+
+    private static bool IsTrashcan(StorageContainer instance)
+    {
+        return instance.gameObject.GetComponent<Trashcan>() != null;
+    }
+
+    [HarmonyPatch(typeof(StorageContainer), nameof(StorageContainer.Awake))]
+    [HarmonyPrefix]
+    private static void StorageContainer_Awake(StorageContainer __instance)
+    {
+        if (Main.Config.EnableCustomContainerSizes && IsTrashcan(__instance))
+        {
+            var width = Main.Config.TrashcanStorageWidth;
+            var height = Main.Config.TrashcanStorageHeight;
+
+            if (width > 0 && height > 0) __instance.Resize(width, height);
+        }
+    }
+
+    [HarmonyPatch(typeof(Trashcan), nameof(Trashcan.OnEnable))]
+    [HarmonyPostfix]
+    public static void Trashcan_OnEnable(Trashcan __instance)
+    {
+        __instance.startDestroyTimeOut = Main.Config.TrashcanDestroyDelay;
+        __instance.destroyInterval = Main.Config.TrashcanDestroyInterval;
+    }
+
+    #endregion
+
+    #region Water Filtration Machine
+
+    [HarmonyPatch(typeof(FiltrationMachine), nameof(FiltrationMachine.Start))]
+    [HarmonyPostfix]
+    private static void FiltrationMachine_Start(FiltrationMachine __instance)
+    {
+        if (!Main.Config.EnableCustomContainerSizes) return;
+
+        var totalSlots = Main.Config.WFMStorageWidth * Main.Config.WFMStorageHeight;
+
+        // Adjust max water/salt
+        var requestedTotal = Main.Config.WFMMaxWaterBottles + Main.Config.WFMMaxSalt;
+        if (requestedTotal > totalSlots)
+        {
+            var ratio = (float)totalSlots / requestedTotal;
+            __instance.maxWater = Mathf.FloorToInt(Main.Config.WFMMaxWaterBottles * ratio);
+            __instance.maxSalt = Mathf.FloorToInt(Main.Config.WFMMaxSalt * ratio);
+        }
+        else
+        {
+            __instance.maxWater = Main.Config.WFMMaxWaterBottles;
+            __instance.maxSalt = Main.Config.WFMMaxSalt;
+        }
+
+        __instance.storageContainer.Resize(Main.Config.WFMStorageWidth, Main.Config.WFMStorageHeight);
+    }
+
+    #endregion
+}
