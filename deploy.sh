14531269e0e86b2b93281a05dbf3169b1d546b30
Ungeziefi <163609976+Ungeziefi@users.noreply.github.com>
2026-05-05 19:14:26 +0200

    Update deploy.sh for Arch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # $1 is TargetName, $2 is TargetPath, $3 is SolutionDir
-GAME_DIR="/home/osema/.steam/debian-installation/steamapps/common/Subnautica"
+GAME_DIR="/home/osema/.local/share/Steam/steamapps/common/Subnautica/"
 
 mkdir -p "$GAME_DIR/BepInEx/plugins/$1"
 cp -f "$2" "$GAME_DIR/BepInEx/plugins/$1"
